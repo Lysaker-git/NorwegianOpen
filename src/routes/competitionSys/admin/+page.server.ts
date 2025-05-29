072bb133322d4dc41b2c46fdb961e635c77354cc
Lysaker-git <lysakerwcs@hotmail.com>
2025-05-29 11:30:00 +0200

    commented out code

--- src/routes/competitionSys/admin/+page.server.ts
+++ src/routes/competitionSys/admin/+page.server.ts
@@ -2,32 +2,32 @@
 import { json } from '@sveltejs/kit';
 import { supabaseAdmin } from '$lib/supabaseAdminClient';
 
-export async function GET({ request }) {
-  try {
-    const { data, error } = await supabaseAdmin
-      .from('competitions')
-      .select('*')
-      .order('date', { ascending: false });
+// export async function GET({ request }) {
+//   try {
+//     const { data, error } = await supabaseAdmin
+//       .from('competitions')
+//       .select('*')
+//       .order('date', { ascending: false });
       
-    if (error) throw error;
-    return json(data);
-  } catch (error) {
-    return json({ error: error.message }, { status: 500 });
-  }
-}
+//     if (error) throw error;
+//     return json(data);
+//   } catch (error) {
+//     return json({ error: error.message }, { status: 500 });
+//   }
+// }
 
-export async function POST({ request }) {
-  try {
-    const competition = await request.json();
+// export async function POST({ request }) {
+//   try {
+//     const competition = await request.json();
     
-    const { data, error } = await supabaseAdmin
-      .from('competitions')
-      .insert([competition])
-      .select();
+//     const { data, error } = await supabaseAdmin
+//       .from('competitions')
+//       .insert([competition])
+//       .select();
       
-    if (error) throw error;
-    return json(data[0]);
-  } catch (error) {
-    return json({ error: error.message }, { status: 500 });
-  }
-}
+//     if (error) throw error;
+//     return json(data[0]);
+//   } catch (error) {
+//     return json({ error: error.message }, { status: 500 });
+//   }
+// }
