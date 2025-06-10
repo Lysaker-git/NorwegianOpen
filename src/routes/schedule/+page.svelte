f23c409d74f7a44e741668f767629446f505c3f6
Lysaker-git <lysakerwcs@hotmail.com>
2025-06-10 22:12:22 +0200

    added mail for pay confirm - and a small footnote in dashboard about how many has paid.

--- src/routes/schedule/+page.svelte
+++ src/routes/schedule/+page.svelte
@@ -1,7 +1,18 @@
-<div class="grid place-items-center h-[60vh] w-full">
-    <h1 class="text-6xl text-center text-white">
-        Schedule not ready yet
-    </h1>
+<script lang="ts">
+    import preliminarySchedule from '$lib/components/preliminarySchedule.jpg';
+</script>
+
+<div class="px-4 sm:px-6 md:px-8 max-w-5xl mx-auto mt-8">
+    <div class="p-4">
+        <img 
+            src={preliminarySchedule} 
+            alt="Preliminary Schedule" 
+            class="w-full rounded-lg shadow-md" 
+        />
+        <p class="mt-4 text-lg text-gray-300 text-center">
+            Please note that this schedule is subject to change. Stay tuned for updates!
+        </p>
+    </div>
 </div>
 
 <style>
